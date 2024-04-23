0490580f559ebec1119f7f154f44cb33ba1e4ae0
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-04-23 12:42:03 -0400

    initial commitial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/************************************************************************
+ * TODO
+ * Create a button component and nest it inside of main app
+ * Display dynamic data
+ * Use conditional rendering
+ * Simulate api requests and responses
+ * Manage user interaction and state
+ * Create functions to handle user interaction
+ ************************************************************************/
+import { billBoardChartData } from "./lib/data";
+
+function Home() {
+  return <></>;
+}
+
+export default Home;
---
+++ src/lib/data.js
@@ -0,0 +1,104 @@
+// Information as of 4-23-24
+
+export const billBoardChartData = [
+    {
+        id: 1,
+        title: "Too Sweet",
+        artists: ["Hozier"],
+        album: "Unheard",
+        releaseDate: 2024,
+        weeksOnChart: 4,
+        albumAvailable: true,
+        imgSrc: "https://upload.wikimedia.org/wikipedia/en/9/9a/Hozier_-_Unheard.png"
+    },
+    {
+        id: 2,
+        title: "Like That",
+        artists: ["Future", "Metro Boomin", "Kendrick Lamar"],
+        album: "We Don't Trust You",
+        releaseDate: 2024,
+        weeksOnChart: 4,
+        albumAvailable: true,
+        imgSrc: "https://townsquare.media/site/812/files/2024/03/attachment-future-metro-boomin-we-dont-trust-you-photo.jpg?w=1080&q=75"
+    },
+    {
+        id: 3,
+        title: "Beautiful Things",
+        artists: ["Benson Boone"],
+        album: "Fireworks & Rollerblades",
+        releaseDate: 2023,
+        weeksOnChart: 13,
+        albumAvailable: false,
+        imgSrc: "https://images.genius.com/69394bd028bd462fa75ff9e69ac868ee.1000x1000x1.png"
+    },
+    {
+        id: 4,
+        title: "Lose Control",
+        artists: ["Teddy Swims"],
+        album: "I've tried everything but Therapy (Part 1)",
+        releaseDate: 2023,
+        weeksOnChart: 36,
+        albumAvailable: true,
+        imgSrc: "https://upload.wikimedia.org/wikipedia/en/0/03/Teddy_Swims_-_Lose_Control.jpg"
+    },
+    {
+        id: 5,
+        title: "Texas Hold 'Em",
+        artists: ["Beyonce"],
+        album: "Cowboy Carter",
+        releaseDate: 2024,
+        weeksOnChart: 10,
+        albumAvailable: true,
+        imgSrc: "https://media.pitchfork.com/photos/65f9ba5e7f6a6f4c6c74a9d8/master/pass/Beyonce-Cowboy-Carter.jpg"
+    },
+    {
+        id: 6,
+        title: "Lovin On Me",
+        artists: ["Jack Harlow"],
+        album: "JH4*",
+        releaseDate: 2023,
+        weeksOnChart: 23,
+        albumAvailable: false,
+        imgSrc: "https://upload.wikimedia.org/wikipedia/en/7/72/%22Lovin_On_Me%22_by_Jack_Harlow_-_cover_art.png"
+    },
+    {
+        id: 7,
+        title: "Espresso",
+        artists: ["Sabrina Carpenter"],
+        album: "",
+        releaseDate: 2024,
+        weeksOnChart: 1,
+        albumAvailable: false,
+        imgSrc: "https://s3.amazonaws.com/media.thecrimson.com/photos/2024/04/17/140839_1370608.jpg"
+    },
+    {
+        id: 8,
+        title: "We Can't Be Friends (Wait For Your Love)",
+        artists: ["Ariana Grande"],
+        album: "Eternal Sunshine",
+        releaseDate: 2024,
+        weeksOnChart: 6,
+        albumAvailable: true,
+        imgSrc: "https://images.genius.com/fffe55f090ff1c76aa091b656d3a0d6a.1000x1000x1.jpg"
+    },
+    {
+        id: 9,
+        title: "Stick Season",
+        artists: ["Noah Kahan"],
+        album: "Stick Season (Forever)",
+        releaseDate: 2024,
+        weeksOnChart: 29,
+        albumAvailable: true,
+        imgSrc: "https://upload.wikimedia.org/wikipedia/en/1/1b/Noah_Kahan_-_Stick_Season_%28song%29.png"
+    },
+    {
+        id: 10,
+        title: "Saturn",
+        artists: ["SZA"],
+        album: "Lana",
+        releaseDate: 2024,
+        weeksOnChart: 8,
+        albumAvailable: false,
+        imgSrc: "https://upload.wikimedia.org/wikipedia/en/c/c8/SZA_-_Saturn.png"
+    },
+]
